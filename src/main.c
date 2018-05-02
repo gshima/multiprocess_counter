0734849787ea72944854303532d6a1c83f8dea41
Gabriela Akemi Shima <gabrielashima@gmail.com>
2018-05-02 20:42:36 -0300

    Adicionada função que verifica se o número inteiro é primo ou não.

--- src/main.c
+++ src/main.c
@@ -3,20 +3,42 @@
  *
  * Este programa recebera uma serie de inteiros em sua entrada. Ao receber
  * um caractere fim de linha ('\n'), deve imprimir na tela a quantidade de
- * números primos da lista da entrada.
+ * números primos da lista da entrada utilizando multiprocessos
  *
  */
 
  #include <stdio.h>
 
+int calculaPrimo(int numero);
+
  int main() {
- int var;
+ int num;
 
    do {
-     scanf("%d", &var);
-     printf("var = %d\n", var);
-     
+     scanf("%d", &num);
+     //printf("var = %d\n", var);
+     if( calculaPrimo(num) ){
+        printf("%d é primo!\n", num);
+     }
+     else {
+        printf("%d não é primo!\n",num );
+     }
    } while(getchar() != '\n');
    return 0;
 
  }
+
+ int calculaPrimo(int numero) {
+    int divisoes = 0;
+    for(int i = 1; i <= numero; ++i){
+       if(numero%i == 0) {
+          divisoes++;
+       }
+    }
+    if(divisoes == 2){
+       return 1;
+    }
+    else {
+       return 0;
+    }
+}
